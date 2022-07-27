69d6e75b623709926a6dc980bd21c6e0d52de74c
César severo <direito1boab@gmail.com>
2022-07-27 14:03:30 -0300

    Preparado o ambiente para o pexels

---
+++ app/src/main/java/com/getworld/wallpaperspapeldeparede/SetWalpaperActivity.java
@@ -0,0 +1,14 @@
+package com.getworld.wallpaperspapeldeparede;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetWalpaperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_walpaper);
+    }
+}
